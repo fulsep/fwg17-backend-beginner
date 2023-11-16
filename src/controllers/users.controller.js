cf5979992bd78cc28955b9ab070ed0a24f59f028
fulsep <57238803+fulsep@users.noreply.github.com>
2023-11-16 10:43:06 +0700

    implement create, update, delete with local variable

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,20 +1,91 @@
+let users = [
+    {
+        id: 1,
+        name: 'Leanne Graham'
+    },
+    {
+        id: 2,
+        name: 'Clementine Dubuque'
+    }
+]
+
+let countUser = users.length
+
 exports.getAllUsers = (req, res)=>{
     return res.json({
         success: true,
         message: 'List all users',
-        results: [
-            {
-                id: 1,
-                name: 'Leanne Graham'
-            },
-            {
-                id: 2,
-                name: 'Bob'
-            }
-        ]
+        results: users
     })
 }
 
-// exports.createUser = (req, res)=>{
-//     // process
-// }
+exports.getDetailUser = (req, res)=>{
+    const user = users.filter(item => item.id === parseInt(req.params.id))
+    if(!user[0]){
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        })
+    }
+    
+    return res.json({
+        success: true,
+        message: 'Detail user',
+        results: user[0]
+    })
+}
+
+exports.createUser = (req, res)=>{
+    const {name} = req.body
+    countUser = countUser + 1
+
+    const user = {
+        id: countUser,
+        name
+    }
+
+    users.push(user)
+    return res.json({
+        success: true,
+        message: 'Create user successfully.',
+        results: user
+    })
+
+}
+
+exports.updateUser = (req, res)=>{
+    const {id} = req.params
+    const {name} = req.body
+    const userId = users.map(user => user.id).indexOf(parseInt(id))
+    if(userId === -1){
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        })
+    }
+    
+    users[userId].name = name
+    return res.json({
+        success: true,
+        message: 'OK',
+        results: users[userId]
+    })
+}
+
+exports.deleteUser = (req, res)=>{
+    const {id} = req.params
+    const user = users.filter(user => user.id === parseInt(id)) 
+    if(!user.length){
+        return res.status(404).json({
+            success: false,
+            message: 'User not found'
+        })
+    }
+    
+    users = users.filter(user => user.id !== parseInt(id))
+    return res.json({
+        success: true,
+        message: 'Delete success',
+        results: user[0]
+    })
+}
